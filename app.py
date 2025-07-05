3f89b833a4da5bd66a9c06b1859c799393b3731e
Pranav Atote <123180829+PranavAI2050@users.noreply.github.com>
2025-07-05 14:10:16 +0545

    Update app.py

--- app.py
+++ app.py
@@ -58,3 +58,8 @@ def extract_text():
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({"status": "API is running"}), 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
+
